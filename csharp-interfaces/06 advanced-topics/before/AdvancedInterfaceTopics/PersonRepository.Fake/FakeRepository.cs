1250b0101020ec5bf64c2c672d679c6b6631d48e
van pham <van.pham@niteco.se>
2018-10-02 22:40:06 +0700

    Add source code

---
+++ csharp-interfaces/06 advanced-topics/before/AdvancedInterfaceTopics/PersonRepository.Fake/FakeRepository.cs
@@ -0,0 +1,49 @@
+﻿using PersonRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonRepository.Fake
+{
+    public class FakeRepository : IPersonRepository
+    {
+        public IEnumerable<Person> GetPeople()
+        {
+            var people = new List<Person>()
+            {
+                new Person() {FirstName = "John", LastName = "Smith", 
+                    Rating = 7, StartDate = DateTime.Parse("10/01/2000")},
+                new Person() {FirstName = "Mary", LastName = "Thomas", 
+                    Rating = 9, StartDate = DateTime.Parse("07/23/1971")},
+            };
+            return people;
+        }
+
+        public Person GetPerson(string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddPerson(Person newPerson)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePerson(string lastName, Person updatedPerson)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePerson(string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePeople(IEnumerable<Person> updatedPeople)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
